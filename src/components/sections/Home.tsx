90d01b52ff8eaa64c316430b91c1a2ba1ca74f78
Jonathan Payawal <jonathanpayawaru@gmail.com>
2025-08-03 14:42:19 +0800

    fix: update Resume link to point to Google Docs and ensure proper target attributes

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -29,7 +29,9 @@ export const Home: React.FC<HomePropTypes> = ({ data, loading }) => {
 
               <div className="flex justify-center space-x-4">
                 <a
-                  href="#projects"
+                  href="https://docs.google.com/document/d/1-pz_YzoSsHztJAx3VBhtR955AykBnQxF/edit?usp=sharing&ouid=111632847173830128702&rtpof=true&sd=true"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
                 >
                   Resume
@@ -39,7 +41,7 @@ export const Home: React.FC<HomePropTypes> = ({ data, loading }) => {
                   target="_blank"
                   rel="noopener noreferrer"
                   className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
-            hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
+                             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
                 >
                   LinkedIn
                 </a>
