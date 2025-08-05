6bf4a7e04b546c6bf466c9bc0f0504d168a46e78
Jonathan Payawal <jonathanpayawaru@gmail.com>
2025-08-05 19:23:26 +0800

    feat: wrap App component with HashRouter for improved routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
 );
